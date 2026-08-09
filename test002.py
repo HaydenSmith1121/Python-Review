947b79422e261ecec8777f00b90fdf9408458ae7
zzc <zhaozhichengyyds@gmail.com>
2026-08-09 17:49:34 +0800

    零基础开始

--- test002.py
+++ test002.py
@@ -1,1 +1,17 @@
 print("hello world")
+# python对缩进敏感
+
+#数据类型
+# 1.int
+# 2.str
+# 3.float
+# 4.bool :True False
+# 5.list
+# 6.tuple
+# 7.dict
+# 8.set
+
+a=100
+b=1.2
+c=True
+print(type(a),type(b),type(c))
