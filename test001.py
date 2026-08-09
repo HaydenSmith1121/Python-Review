b6d35db3835d6594a50a34b6cb9c29f3c822e6d4
zzc <zhaozhichengyyds@gmail.com>
2026-08-09 17:51:02 +0800

    第一天复习内容

---
+++ test001.py
@@ -0,0 +1,18 @@
+print("hello world")
+# python对缩进敏感
+
+#数据类型
+# 1.int
+# 2.str
+# 3.float
+# 4.bool :True False
+# 5.list
+# 6.tuple
+# 7.dict
+# 8.set
+
+a=100
+b=1.2
+c=True
+d="abc123"
+print(type(a),type(b),type(c),type(d))
